2ebcfc5551b527e7aa164b2933440489365f5c98
Nico D <ndemarte3907@student.pps.net>
2022-10-06 09:52:09 -0700

    crearted sphere

---
+++ Assets/scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+
+
+        Destroy(gameObject);
+    }
+
+
+
+}
