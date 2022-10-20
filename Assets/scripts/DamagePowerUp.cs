df9d8662416692e456a34d40b1880c9ff8e37ebc
Nico D <ndemarte3907@student.pps.net>
2022-10-20 09:22:08 -0700

    made another power up i guess

--- Assets/scripts/DamagePowerUp.cs
+++ Assets/scripts/DamagePowerUp.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour{
-
+public class DamagePowerUp : MonoBehaviour
+{
     public float multiplier = 1.4f;
     public float duration = 4f;
 
@@ -14,25 +14,25 @@ public class PowerUp : MonoBehaviour{
     {
         if (other.CompareTag("Player"))
         {
-            StartCoroutine( Pickup(other) );
+            StartCoroutine(Pickup(other));
         }
     }
 
     IEnumerator Pickup(Collider player)
     {
         //Debug.Log("Power up picked up!");
-        
+
         //Instantiate(pickupEffect, transform.position, transform.rotation);
 
         PlayerStats stats = player.GetComponent<PlayerStats>();
-        stats.health *= multiplier;
+        stats.damage *= multiplier;
 
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<Collider>().enabled = false;
 
         yield return new WaitForSeconds(duration);
 
-        stats.health /= multiplier;
+        stats.damage /= multiplier;
 
         Destroy(gameObject);
     }
--- Assets/scripts/PlayerStats.cs
+++ Assets/scripts/PlayerStats.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour{
 
     public float health = 100f;
+    public float damage = 100f;
+
+
+
 
 }
