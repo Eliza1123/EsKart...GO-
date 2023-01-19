d28f670bd2108482acf63f904e092ee4b5f21ddb
Eliza S <esnell1992@student.pps.net>
2023-01-19 09:17:19 -0800

    Mandell test push

--- Assets/scripts/CarDrive.cs
+++ Assets/scripts/CarDrive.cs
@@ -11,7 +11,7 @@ public class CarDrive : MonoBehaviour
 
     private Rigidbody rb;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update!
     void Start()
     {
         rb = GetComponent<Rigidbody>();
